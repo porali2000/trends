037e051f8dffe7d673050efc851ab8df93e9a8a7
Porali <porali2000s@gmail.com>
2018-10-26 20:36:52 -0700

    Full Scale Update

---
+++ fn/fnTrends/fnInterestOverTime.js
@@ -0,0 +1,51 @@
+exports.handler = async(event) => {
+    const googleTrends = require('google-trends-api');
+    var key = event['queryStringParameters']['keyword'];
+    var city = event['queryStringParameters']['city'];
+    var status = 200;
+    var toReturn = 'Ok';
+
+    console.log('keyword', key);
+    console.log('city', city);
+    console.log('startdate', startdate);
+    console.log('enddate', enddate);
+    if ('undefined' != event['queryStringParameters']['startdate'] &&
+        null != event['queryStringParameters']['startdate']) {
+        var startdate = new Date(event['queryStringParameters']['startdate']);
+        var enddate = new Date(event['queryStringParameters']['enddate']);
+        var pro = await googleTrends.interestOverTime({
+                keyword: key,
+                startTime: startdate,
+                endTime: enddate,
+                resolution: city
+            })
+            .then((res) => {
+                toReturn = res;
+                status = 200;
+            })
+            .catch((err) => {
+                console.error(err);
+                toReturn = "{'Error' :'" + err + "'}";
+                status = 400;
+            });
+    }
+    else {
+        var pro = await googleTrends.interestOverTime({
+                keyword: key
+            })
+            .then((res) => {
+                toReturn = res;
+                status = 200;
+            })
+            .catch((err) => {
+                console.error(err);
+                toReturn = "{'Error' :'" + err + "'}";
+                status = 400;
+            });
+    }
+    const response = {
+        statusCode: status,
+        body: JSON.stringify(toReturn)
+    };
+    return response;
+}
